728d14a222450ac948a243d88eb72c835bcba449
SUNITHAVARMA <sunitha.pachamatla26@gmail.com>
2019-06-11 17:54:52 +0530

    added docker-compose.yml and parent pom

--- muzix-service/src/main/java/com/stackroute/muzixservice/domain/Track.java
+++ muzix-service/src/main/java/com/stackroute/muzixservice/domain/Track.java
@@ -7,13 +7,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
-@Document
+@Document(collection = "track")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Track {
     @Id
+
     private String id;
     private String name;
     private String description;
